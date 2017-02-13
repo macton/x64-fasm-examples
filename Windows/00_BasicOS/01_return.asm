ddf9e3921aa78af40ea32a2b557a63f31a16e6a5
Mike Acton <macton@gmail.com>
2017-02-13 10:07:15 -0800

    remove some unused references

--- Windows/00_BasicOS/01_return.asm
+++ Windows/00_BasicOS/01_return.asm
@@ -1,3 +1,7 @@
+; 01_return.asm
+; Simple fasm example. Returns 42.
+;   - Why 42? Check out: http://www.muppetlabs.com/~breadbox/software/tiny/return42.html
+
 format PE64 console 
 entry start 
 include 'win64a.inc' 
@@ -12,14 +16,7 @@ start:
 
 section '.idata' import data readable writeable 
 
-library kernel32,'kernel32.dll',\ 
-        msvcrt,  'msvcrt.dll' 
+library kernel32,'kernel32.dll'
 
 import kernel32,\ 
-       ExitProcess,       'ExitProcess', \
-       GetModuleFileName, 'GetModuleFileNameA' 
-
-import msvcrt, \ 
-       printf, 'printf', \
-       strlen, 'strlen', \
-       scanf,  'scanf' 
+  ExitProcess,       'ExitProcess'
